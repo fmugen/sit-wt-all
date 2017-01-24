6570f2cddc4349f40de71485da9bd85f96c9a60f
kumakuratk <takuya.kumakura@monocrea.jp>
2017-01-24 13:22:13 +0900

    RV指摘のフィードバック
    DDL/DML実行時の例外処理を実装
    Derbyディレクトリが存在する場合は削除
    異常系のassertを実装

--- sit-wt-runtime/src/test/java/org/sitoolkit/wt/domain/operation/selenium/DbVerifyOperationTest.java
+++ sit-wt-runtime/src/test/java/org/sitoolkit/wt/domain/operation/selenium/DbVerifyOperationTest.java
@@ -1,5 +1,8 @@
 package org.sitoolkit.wt.domain.operation.selenium;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
 import java.io.File;
 import java.nio.charset.Charset;
 import java.sql.Connection;
@@ -17,12 +20,19 @@ import org.sitoolkit.wt.domain.tester.SitTesterTestBase;
 public class DbVerifyOperationTest extends SitTesterTestBase {
 
     @BeforeClass
-    public static void runDerby() {
-        String derbyDir = "target/derby/testdb";
+    public static void runDerby() throws Exception {
         Connection conn = null;
         Boolean result = null;
 
         try {
+            String derbyDir = "target/derby/testdb";
+            File beforeDir = new File(derbyDir);
+            if (beforeDir.exists()) {
+                if (!rmTree(beforeDir)) {
+                    throw new Exception(derbyDir + " delete failed.");
+                }
+            }
+
             conn = DriverManager.getConnection("jdbc:derby:" + derbyDir + ";create=true");
             Statement state = conn.createStatement();
 
@@ -34,7 +44,7 @@ public class DbVerifyOperationTest extends SitTesterTestBase {
             }
 
         } catch (Exception exp) {
-            exp.printStackTrace();
+            throw exp;
         }
     }
 
@@ -45,32 +55,108 @@ public class DbVerifyOperationTest extends SitTesterTestBase {
 
     @Test
     public void test002() {
-        test();
+        try {
+            test();
+        } catch (AssertionError err) {
+            int expCnt = 0;
+            for (String line : err.getMessage().split("[\r\n]")) {
+                if (line.indexOf("Exception") != -1) {
+                    expCnt++;
+                    assertThat(line, startsWith("\torg.sitoolkit.wt.infra.VerifyException"));
+                }
+            }
+            assertThat(expCnt, is(4));
+        }
     }
 
     @Test
     public void test003() {
-        test();
+        try {
+            test();
+        } catch (AssertionError err) {
+            int expCnt = 0;
+            for (String line : err.getMessage().split("[\r\n]")) {
+                if (line.startsWith("Caused by:")) {
+                    expCnt++;
+                    assertThat(line, startsWith(
+                            "Caused by: org.springframework.dao.EmptyResultDataAccessException"));
+                }
+            }
+            assertThat(expCnt, is(1));
+        }
     }
 
     @Test
     public void test004() {
-        test();
+        try {
+            test();
+        } catch (AssertionError err) {
+            int expCnt = 0;
+            for (String line : err.getMessage().split("[\r\n]")) {
+                if (line.startsWith("Caused by:")) {
+                    expCnt++;
+                    assertThat(line, startsWith(
+                            "Caused by: org.springframework.dao.IncorrectResultSizeDataAccessException"));
+                }
+            }
+            assertThat(expCnt, is(1));
+        }
     }
 
     @Test
     public void test005() {
-        test();
+        try {
+            test();
+        } catch (AssertionError err) {
+            int expCnt = 0;
+            for (String line : err.getMessage().split("[\r\n]")) {
+                if (line.startsWith("Caused by:")) {
+                    expCnt++;
+                    assertThat(line, startsWith(
+                            "Caused by: org.springframework.dao.InvalidDataAccessApiUsageException"));
+                }
+            }
+            assertThat(expCnt, is(1));
+        }
     }
 
     @Test
     public void test006() {
-        test();
+        try {
+            test();
+        } catch (AssertionError err) {
+            int expCnt = 0;
+            for (String line : err.getMessage().split("[\r\n]")) {
+                if (line.startsWith("Caused by:")) {
+                    expCnt++;
+                    if (expCnt == 1) {
+                        assertThat(line, startsWith(
+                                "Caused by: org.springframework.jdbc.BadSqlGrammarException"));
+                    } else if (expCnt == 2) {
+                        assertThat(line, startsWith("Caused by: java.sql.SQLSyntaxErrorException"));
+                    } else if (expCnt == 3) {
+                        assertThat(line, startsWith("Caused by: ERROR 42X01"));
+                    }
+                }
+            }
+            assertThat(expCnt, is(3));
+        }
     }
 
     @Test
     public void test007() {
-        test();
+        try {
+            test();
+        } catch (AssertionError err) {
+            int expCnt = 0;
+            for (String line : err.getMessage().split("[\r\n]")) {
+                if (line.startsWith("Caused by:")) {
+                    expCnt++;
+                    assertThat(line, startsWith("Caused by: java.io.FileNotFoundException"));
+                }
+            }
+            assertThat(expCnt, is(1));
+        }
     }
 
     @Override
@@ -81,5 +167,32 @@ public class DbVerifyOperationTest extends SitTesterTestBase {
     @Override
     protected String getSheetName() {
         return "TestScript";
+    }
+
+    public static boolean rmTree(File target) {
+        boolean result = false;
+
+        if (target.isDirectory()) {
+            int delCnt = 0;
+            boolean childResult = false;
+
+            for (File child : target.listFiles()) {
+                delCnt++;
+                childResult = rmTree(child);
+
+                if (!childResult) {
+                    result = childResult;
+                    break;
+                }
+            }
+
+            if (childResult || delCnt == 0) {
+                result = target.delete();
+            }
+        } else {
+            result = target.delete();
+        }
+
+        return result;
     }
 }
